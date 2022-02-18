bcb714cd57e2a113baed1a4997191bce4f1d2d9a
vo1200 <88854148+vo120@users.noreply.github.com>
2022-02-18 15:55:32 -0500

    -website completed (might need to go in and fix prev and nav algo a bit for images)

--- main.js
+++ main.js
@@ -33,3 +33,51 @@ TweenMax.staggerFrom("#menu li a", 1, {
 //nav animation ends here
 
 new WOW().init();
+
+// prev and next toggle sections for project page starts here
+
+$(document).ready(function(){
+    $(".projpage div.hero-content").each(function(e) {
+        if (e != 0)
+            $(this).hide();
+    });
+
+    $("#next").click(function(){
+        if ($(".projpage div.hero-content:visible").next().length != 0)
+            $(".projpage div.hero-content:visible").next().show().prev().hide();
+        else {
+            $(".projpage div.hero-content:visible").hide();
+            $(".projpage div.hero-content:first").show();
+        }
+        return false;
+    });
+
+    $("#prev").click(function(){
+        if ($(".projpage div.hero-content:visible").prev().length != 0)
+            $(".projpage div.hero-content:visible").prev().show().next().hide();
+        else {
+            $(".projpage div.hero-content:visible").hide();
+            $(".projpage div.hero-content:last").show();
+        }
+        return false;
+    });
+});
+
+let active = 0;
+let el = $("div.rowImg");
+let total = el.length - 1;
+
+const toggle = (el, index) => {
+    el.hide()
+    el.eq(index).show()
+}
+
+toggle(el, active)
+
+$('p').on('click', function() {
+    this.id === 'prev' ? active -= 1 : active += 1;
+    if(active > total) active = 0;
+    if(active < 0) active = total
+    toggle(el, active)
+})
+//prev and next toggle sections for project page ends here
