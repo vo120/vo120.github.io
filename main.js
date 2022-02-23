ad13df467d56acab431f939de22e1f271323c158
vo1200 <88854148+vo120@users.noreply.github.com>
2022-02-22 23:24:21 -0500

    -website completed (prev and nav buttons working better, font weight heavier where needed)

--- main.js
+++ main.js
@@ -36,7 +36,57 @@ new WOW().init();
 
 // prev and next toggle sections for project page starts here
 
-$(document).ready(function(){
+/*let linkArr = ["project1.html", "project2.html", "project3.html", "project4.html", "project5.html"]
+
+$('.prev').on('click', function() {
+    let currentSrc = $('.iframeWindow').attr('src')
+    if (linkArr.indexOf(currentSrc) != 0) {
+        $('.iframeWindow').attr('src', linkArr[linkArr.indexOf(currentSrc) - 1])
+    }
+
+})
+
+$('.next').on('click', function() {
+    let currentSrc = $('.iframeWindow').attr('src')
+    if (linkArr.indexOf(currentSrc) != 4) {
+        $('.iframeWindow').attr('src', linkArr[linkArr.indexOf(currentSrc) + 1])
+    }
+})*/
+
+/*function getNewUrl(addNum) {
+    var url = window.location.pathname;
+    var lastIndex = url.lastIndexOf('/project1');
+    var filename = url.substring(lastIndex + 1);
+    var newFileName = filename.substring(0, 2) +
+        pad(parseInt(filename.substring(2, 5), 10) + addNum, 3) +
+        filename.substring(5);
+    return url.substring(0, lastIndex) + newFileName;
+}
+
+function pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+    return str;
+}*/
+/*var pages = ['project1.html','project2.html','project3.html','project4.html', 'project5.html'];
+
+$(".next").on("click", function() {
+    for(var i = 0; i < pages.length; i++){
+        if(window.location.pathname === pages[i]){
+            if(i === pages.length - 1){
+                //at the end, go to first page
+                window.location.href=pages[0];
+            }
+            else{
+                window.location.href=pages[i+1];
+            }
+            break;
+        }
+    }
+});*/
+/*$(document).ready(function(){
     $(".projpage div.hero-content").each(function(e) {
         if (e != 0)
             $(this).hide();
@@ -61,9 +111,9 @@ $(document).ready(function(){
         }
         return false;
     });
-});
+});*/
 
-let active = 0;
+/*let active = 0;
 let el = $("div.rowImg");
 let total = el.length - 1;
 
@@ -79,5 +129,5 @@ $('p').on('click', function() {
     if(active > total) active = 0;
     if(active < 0) active = total
     toggle(el, active)
-})
+})*/
 //prev and next toggle sections for project page ends here
